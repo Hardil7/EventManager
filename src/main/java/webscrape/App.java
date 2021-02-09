40751cea5c847d105f740718b0b74ca34340caf0
hardil7 <mehtahardil@gmail.com>
2021-02-09 16:11:06 -0500

    Completed fetching events.

--- src/main/java/webscrape/App.java
+++ src/main/java/webscrape/App.java
@@ -3,7 +3,9 @@ package webscrape;
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -26,46 +28,34 @@ public class App
             // With the document fetched, we use JSoup's title() method to fetch the title
             System.out.printf("Title: %s\n", doc.title());
       
-            // Get the list of repositories
-            Elements repositories = doc.select("tr");
+            // Get the list of Events
+            Elements events = doc.select("tr");
             
-          // In case of any IO errors, we want the messages written to the console
-
+            ArrayList<String> event = new ArrayList<>();
+            ArrayList<String> description = new ArrayList<>();
+			List<Date> startDate = new ArrayList<Date>();
+            List<Date> endDate = new ArrayList<Date>();
         
-
-       	// Convenience class for writing character files
-			FileWriter writer;
-			writer = new FileWriter("document.txt", false);
-            String event[] = new String[repositories.size()];
-            String description[] = new String[repositories.size()];
-			Date startDate[] = new Date[repositories.size()];
-            // Writes text to a character-output stream
-            int i =0 ;
-			for(Element repo : repositories){
-                event[i++] =  repo.select("a").attr("title").toString();
-                String td = repo.select("td").text();
-                System.out.println(td + "\n\n");
-                /*try{
-                    startDate[i] = new SimpleDateFormat("dd/MM/yyyy").parse(repo.select("td").get(2).data());  
+			for(Element eve : events){
+                if(eve.select("a").attr("title") != ""){   
+                    event.add(eve.select("a").attr("title").toString());
+                    description.add(eve.select("td").get(0).text());
+                    try{
+                        startDate.add(new SimpleDateFormat("yyyy-MM-dd").parse(eve.select("td").get(1).text()));  
+                    }
+                    catch(ParseException exception){
+                        System.out.println("Unable to save the start date " +eve.select("td").get(1).text() );
+                    }
+                    try{
+                        endDate.add(new SimpleDateFormat("yyyy-MM-dd").parse(eve.select("td").get(2).text()));  
+                    }
+                    catch(ParseException exception){
+                        System.out.println("Unable to save th end date " +eve.select("td").get(2).text() );
+                    }
                 }
-                catch(ParseException exception){
-                    System.out.println("Unable to save the date " +repo.select("td").get(2).data() );
-                }
-                */
-            }
-            for(int j = 0 ; i<event.length;i++){
-                System.out.println("\n\nEvent " + j);
-                System.out.println("Title: " + event[i-1]);
-               /* System.out.println("" + description[i-1]);
-                System.out.println("Date: "+ startDate[i-1].toString());*/
-
-            } 
-
-            BufferedWriter bufferWriter = new BufferedWriter(writer);
-            bufferWriter.close();
-            
-		} catch (IOException e) {
-			
+            }     
+		} catch (Exception e) {
+			System.out.printf("Error Stack:" + e.getMessage());
 		}
     }
 }
